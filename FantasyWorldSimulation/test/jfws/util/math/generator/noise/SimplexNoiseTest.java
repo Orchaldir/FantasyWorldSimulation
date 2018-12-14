03018f93f526d199bb56b4c3109b50e1fd091eb3
Orchaldir <orchaldir@gmail.com>
2018-12-14 22:44:52 +0100

    #28: Improved SimplexNoiseTest.

--- FantasyWorldSimulation/test/jfws/util/math/generator/noise/SimplexNoiseTest.java
+++ FantasyWorldSimulation/test/jfws/util/math/generator/noise/SimplexNoiseTest.java
@@ -3,6 +3,7 @@ package jfws.util.math.generator.noise;
 import jfws.util.math.generator.noise.SimplexNoise;
 import lombok.extern.slf4j.Slf4j;
 import org.hamcrest.core.AllOf;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,36 +19,36 @@ class SimplexNoiseTest {
 
 	private SimplexNoise noise = new SimplexNoise();
 
-	// floorFast()
+	@Nested
+	class TestFloorFast {
 
-	@Test
-	public void testFloorFastWithInteger() {
-		for(int value = -10; value < 10; value++) {
-			assertThat(SimplexNoise.floorFast(value), is(equalTo(value)));
+		@Test
+		public void testWithInteger () {
+			for (int value = -10; value < 10; value++) {
+				assertThat(SimplexNoise.floorFast(value), is(equalTo(value)));
+			}
+		}
+
+		@Test
+		public void testWithPositiveValue () {
+			int integer = 365;
+			double step = 0.01;
+
+			for (double value = 0.0; value < 1.0; value += step) {
+				assertThat(SimplexNoise.floorFast(integer + value), is(equalTo(integer)));
+			}
+		}
+
+		@Test
+		public void testWithNegativeValue () {
+			int integer = -8;
+			double step = 0.01;
+
+			for (double value = 0.0; value < 1.0; value += step) {
+				assertThat(SimplexNoise.floorFast(integer + value), is(equalTo(integer)));
+			}
 		}
 	}
-
-	@Test
-	public void testFloorFastWithPositiveValue() {
-		int integer = 365;
-		double step = 0.01;
-
-		for(double value = 0.0; value < 1.0; value += step) {
-			assertThat(SimplexNoise.floorFast(integer + value), is(equalTo(integer)));
-		}
-	}
-
-	@Test
-	public void testFloorFastWithNegativeValue() {
-		int integer = -8;
-		double step = 0.01;
-
-		for(double value = 0.0; value < 1.0; value += step) {
-			assertThat(SimplexNoise.floorFast(integer + value), is(equalTo(integer)));
-		}
-	}
-
-	// dot()
 
 	@Test
 	public void testDot() {
@@ -56,55 +57,107 @@ class SimplexNoiseTest {
 		assertThat(SimplexNoise.dot(grad, 3.0, 4.0), is(equalTo(11.0)));
 	}
 
-	// generate()
+	@Nested
+	class TestCalculateNoise {
 
-	@Test
-	public void testCalculateNoiseInInterval() {
-		for(double x = -RANGE; x < RANGE; x++) {
-			for(double y = -RANGE; y < RANGE; y++) {
-				double value = noise.generate(x, y);
-				assertThat(value, AllOf.allOf(greaterThan(MIN_VALUE), lessThan(MAX_VALUE)));
-			}
-		}
-	}
-
-	@Test
-	public void testCalculateNoiseMinMax() {
-		double min = MAX_VALUE;
-		double max = MIN_VALUE;
-
-		for(double x = -RANGE; x < RANGE; x++) {
-			for(double y = -RANGE; y < RANGE; y++) {
-				double value = noise.generate(x, y);
-				min = Math.min(min, value);
-				max = Math.max(max, value);
+		@Test
+		public void testInInterval() {
+			for (double x = -RANGE; x < RANGE; x++) {
+				for (double y = -RANGE; y < RANGE; y++) {
+					double value = noise.generate(x, y);
+					assertThat(value, AllOf.allOf(greaterThan(MIN_VALUE), lessThan(MAX_VALUE)));
+				}
 			}
 		}
 
-		assertThat(min, is(closeTo(MIN_VALUE, ERROR)));
-		assertThat(max, is(closeTo(MAX_VALUE, ERROR)));
-	}
+		@Test
+		public void testMinMax() {
+			double min = MAX_VALUE;
+			double max = MIN_VALUE;
 
-	@Test
-	public void testCalculateNoiseAverage() {
-		double average = 0.0;
-		int numbers = 0;
-
-		for(double x = -RANGE; x < RANGE; x++) {
-			for(double y = -RANGE; y < RANGE; y++) {
-				average += noise.generate(x, y);
-				numbers++;
+			for (double x = -RANGE; x < RANGE; x++) {
+				for (double y = -RANGE; y < RANGE; y++) {
+					double value = noise.generate(x, y);
+					min = Math.min(min, value);
+					max = Math.max(max, value);
+				}
 			}
+
+			assertThat(min, is(closeTo(MIN_VALUE, ERROR)));
+			assertThat(max, is(closeTo(MAX_VALUE, ERROR)));
 		}
 
-		average /= numbers;
+		@Test
+		public void testAverage() {
+			double average = 0.0;
+			int numbers = 0;
 
-		assertThat(average, is(closeTo(0, ERROR)));
-	}
+			for (double x = -RANGE; x < RANGE; x++) {
+				for (double y = -RANGE; y < RANGE; y++) {
+					average += noise.generate(x, y);
+					numbers++;
+				}
+			}
 
-	@Test
-	public void testCalculateNoise() {
-		assertThat(noise.generate(1.1, 2.5), is(closeTo(-0.0703, ERROR)));
+			average /= numbers;
+
+			assertThat(average, is(closeTo(0, ERROR)));
+		}
+
+		@Test
+		public void testMaxDiffX() {
+			final double y = 0.0;
+			double oldValue = noise.generate(-RANGE, y);
+			double maxDiff = 0.0;
+			double averageDiff = 0.0;
+			int N = 0;
+
+			for (double x = -RANGE; x < RANGE; x += 0.01) {
+				final double newValue = noise.generate(x, y);
+				final double diff = Math.abs(newValue - oldValue);
+
+				oldValue = newValue;
+				maxDiff = Math.max(maxDiff, diff);
+				averageDiff += diff;
+				N++;
+			}
+
+			averageDiff /= N;
+
+			log.info("testMaxDiffX(): max={} average={}", maxDiff, averageDiff);
+
+			assertThat(maxDiff, is(lessThan(0.1)));
+		}
+
+		@Test
+		public void testMaxDiffY() {
+			final double x = 0.0;
+			double oldValue = noise.generate(x, -RANGE);
+			double maxDiff = 0.0;
+			double averageDiff = 0.0;
+			int N = 0;
+
+			for (double y = -RANGE; y < RANGE; y += 0.01) {
+				final double newValue = noise.generate(x, y);
+				final double diff = Math.abs(newValue - oldValue);
+
+				oldValue = newValue;
+				maxDiff = Math.max(maxDiff, diff);
+				averageDiff += diff;
+				N++;
+			}
+
+			averageDiff /= N;
+
+			log.info("testMaxDiffY(): max={} average={}", maxDiff, averageDiff);
+
+			assertThat(maxDiff, is(lessThan(0.1)));
+		}
+
+		@Test
+		public void testCalculateNoise() {
+			assertThat(noise.generate(1.1, 2.5), is(closeTo(-0.0703, ERROR)));
+		}
 	}
 
 }
