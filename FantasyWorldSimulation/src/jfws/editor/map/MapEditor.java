d20dfcf839c0027658b9db57ca93cfdd1687f24f
Orchaldir <orchaldir@gmail.com>
2019-02-17 08:12:16 +0100

    #45: Reverted project configuration.
    MapEditor.main() could not be executed.

--- FantasyWorldSimulation/src/jfws/editor/map/MapEditor.java
+++ FantasyWorldSimulation/src/jfws/editor/map/MapEditor.java
@@ -6,11 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class MapEditor extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("map-editor.fxml"));
+		URL url = getClass().getResource("map-editor.fxml");
+		Parent root = FXMLLoader.load(url);
 		primaryStage.setTitle("Map Editor");
 		primaryStage.setScene(new Scene(root, 1000, 800));
 		primaryStage.show();
