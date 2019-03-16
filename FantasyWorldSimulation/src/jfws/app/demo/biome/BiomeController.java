ffed7e2f9a315f04a71745b519c1a4bc208c95df
Orchaldir <orchaldir@gmail.com>
2019-03-16 09:21:06 +0100

    #24: Added feature combo box to BiomeDemo.

--- FantasyWorldSimulation/src/jfws/app/demo/biome/BiomeController.java
+++ FantasyWorldSimulation/src/jfws/app/demo/biome/BiomeController.java
@@ -1,7 +1,9 @@
 package jfws.app.demo.biome;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ComboBox;
 import jfws.features.elevation.ElevationColorSelector;
 import jfws.util.math.generator.Transformation;
 import jfws.util.math.generator.noise.SimplexNoise;
@@ -15,6 +17,7 @@ import jfws.util.math.geometry.voronoi.ImageBasedVoronoiDiagram;
 import jfws.util.math.random.GeneratorWithRandom;
 import jfws.util.rendering.CanvasRenderer;
 import jfws.util.rendering.ColorSelector;
+import jfws.util.rendering.RandomColorSelector;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -25,12 +28,24 @@ public class BiomeController {
 	public static final Point2d SIZE = new Point2d(800, 600);
 	public static final Point2d CENTER = SIZE.multiply(0.5);
 
+	enum SelectedFeature {
+		ELEVATION,
+		TEMPERATURE
+	}
+
 	@FXML
 	private Canvas mapCanvas;
 
+	@FXML
+	private ComboBox<SelectedFeature> featureComboBox;
+
+	private SelectedFeature selectedFeature = SelectedFeature.TEMPERATURE;
+
 	private CanvasRenderer canvasRenderer;
 	private MeshRenderer meshRenderer;
-	private ColorSelector<Face> colorSelector = new ElevationColorSelector();
+
+	private ColorSelector elevationColorSelector = new ElevationColorSelector();
+	private RandomColorSelector temperatureColorSelector = new RandomColorSelector(new GeneratorWithRandom(99));
 
 	private final double poissonDiskRadius = 5.0;
 
@@ -53,10 +68,10 @@ public class BiomeController {
 		log.info("createWorldMap(): Init cells");
 
 		SimplexNoise simplexNoise = new SimplexNoise();
-		Transformation gradient = new Transformation(simplexNoise, -50.0, 175.0, 200);
+		Transformation elevationGenerator = new Transformation(simplexNoise, -50.0, 175.0, 200);
 
 		for (Face<Void, Void, Cell> face : voronoiDiagram.getMesh().getFaces()) {
-			double elevation = gradient.generate(points.get(face.getId()));
+			double elevation = elevationGenerator.generate(points.get(face.getId()));
 			face.setData(new Cell(elevation));
 		}
 	}
@@ -64,6 +79,9 @@ public class BiomeController {
 	@FXML
 	private void initialize() {
 		log.info("initialize()");
+
+		featureComboBox.setItems(FXCollections.observableArrayList(SelectedFeature.values()));
+		featureComboBox.getSelectionModel().select(selectedFeature);
 
 		canvasRenderer = new CanvasRenderer(mapCanvas.getGraphicsContext2D());
 		FaceRenderer faceRenderer = new FaceRenderer(canvasRenderer);
@@ -77,6 +95,23 @@ public class BiomeController {
 
 		canvasRenderer.clear(0, 0, 900, 700);
 
+		ColorSelector colorSelector = getColorSelector();
 		meshRenderer.renderFaces(voronoiDiagram.getMesh(), colorSelector);
+	}
+
+	private ColorSelector getColorSelector() {
+		if(selectedFeature == SelectedFeature.ELEVATION) {
+			return elevationColorSelector;
+		}
+		else {
+			return temperatureColorSelector;
+		}
+	}
+
+	@FXML
+	public void onFeatureSelected() {
+		selectedFeature = featureComboBox.getSelectionModel().getSelectedItem();
+		log.info("onFeatureSelected(): {}", selectedFeature);
+		render();
 	}
 }
