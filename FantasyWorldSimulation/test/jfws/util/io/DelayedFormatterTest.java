28b8c538f2945d9ca269d85ab0268e23a123bb04
Orchaldir <orchaldir@gmail.com>
2019-05-04 10:00:15 +0200

    Always use the same locale in DelayedFormatterTest.

--- FantasyWorldSimulation/test/jfws/util/io/DelayedFormatterTest.java
+++ FantasyWorldSimulation/test/jfws/util/io/DelayedFormatterTest.java
@@ -1,12 +1,20 @@
 package jfws.util.io;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 class DelayedFormatterTest {
+
+	@BeforeEach
+	public void setUp() {
+		Locale.setDefault(Locale.ENGLISH);
+	}
 
 	@Test
 	public void testTwoAttributes() {
@@ -15,6 +23,6 @@ class DelayedFormatterTest {
 
 	@Test
 	public void testFormattedFloat() {
-		assertThat(DelayedFormatter.format("Float %.3f", 1.234567).toString(), is(equalTo("Float 1,235")));
+		assertThat(DelayedFormatter.format("Float %.3f", 1.234567).toString(), is(equalTo("Float 1.235")));
 	}
 }
